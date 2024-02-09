2c35e2da14fdb7cce5fcca794538c38eb4fc79c9
varshavijay02 <159517474+varshavijay02@users.noreply.github.com>
2024-02-09 12:51:39 +0530

    Day 1 commit 2

--- Day 1 Training Ex/src/StudentPassOrFail.java
+++ Day 1 Training Ex/src/StudentPassOrFail.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class StudentPassOrFail {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i=0;
+        int failCount=0;
+        int marks;
 
+        System.out.println("Enter 6 subject marks:");
+
+        while(i<6){
+            marks = sc.nextInt();
+            if(marks<45){
+                failCount++;
+            }
+            i++;
+        }
+
+        String[] numWords = {"zero","one","two","three","four","five","six"};
+
+        if(failCount==0) System.out.println("Student Passed!");
+        else System.out.println("Student failed in "+numWords[failCount]+" subjects");
     }
 }
 
-//develop an application to read 6 subjects marks of a student and find out he is passed or failed.   the pass mark for each subject is 45.
+//develop an application to read 6 subjects marks of a student and find out he is passed or failed.
+// the pass mark for each subject is 45.
 //        (use while loop)
 //
 //Input:-
